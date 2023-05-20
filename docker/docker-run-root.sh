a0e7c375cefd41a1c8cd6017b8058d9d65beb583
Vladimir A. Goshev (sunx.me) <sunx_git@sunx.name>
2023-05-20 00:12:00 +0000

    Bump to alpine 3.18 + tiny fixes

--- docker/docker-run-root.sh
+++ docker/docker-run-root.sh
@@ -10,8 +10,8 @@ set -x
 [ -z "$VPN_SUBNET" ] && VPN_SUBNET="10.0.0.0/16"
 
 IPT="/sbin/iptables"
-$IPT -t nat -A POSTROUTING -s $VPN_SUBNET -o eth0 -j MASQUERADE
 $IPT -t nat -N ToVPN
+$IPT -t nat -A POSTROUTING -s $VPN_SUBNET -o eth0 -j MASQUERADE
 $IPT -t nat -A POSTROUTING -d $VPN_SUBNET -o tun0 -j ToVPN
 $IPT -t nat -A ToVPN -s $VPN_SUBNET -j RETURN
 $IPT -t nat -A ToVPN -j MASQUERADE
